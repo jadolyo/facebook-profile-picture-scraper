298da49a1671953ad5e3bc199a29e583ca11e971
Ahmed Abdelrahim M. Jadelrab <ahmad.jadelrab@gmail.com>
2015-10-12 19:59:04 +0200

    Fixed SSL, Cleaned the code and increased image size

--- scraper.php
+++ scraper.php
@@ -30,41 +30,36 @@ $I = PHasher::Instance();
 //This line to prevent execution timeout.
 set_time_limit(0);
 
+//Solving SSL or https problem.
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);
+
 //This is infinte while loop to fetch the number of profile pictures and save it inside avatar folder.
-$initial = 2;
+$initial = 4;
 
 while($fid = $initial){
-// echo "<br />".$fid;
-// $img = @file_get_contents('https://graph.facebook.com/'.$fid.'/picture?type=large');
-	// $data = file_get_contents('https://graph.facebook.com/[App-Scoped-ID]/picture?width=378&height=378&access_token=[Access-Token]');
-	$img = @file_get_contents('https://graph.facebook.com/'.$fid.'/picture?width=378&height=378');
+
+	$img = file_get_contents('https://graph.facebook.com/'.$fid.'/picture?width=378&height=378', false, stream_context_create($arrContextOptions));
 	$file = dirname(__file__).'/avatar/'.$fid.'.jpg';
 	file_put_contents($file, $img);
-
-	//echo '<a href="https://www.facebook.com/profile.php?id='.$fid.'"><img src="avatar/'.$fid.'.jpg" /></a><br />';
 
 	//PHasher class to hash the images to hexdecimal values or binary values.
 	$hash = $I->FastHashImage($file);
 	$hex = $I->HashAsString($hash);
-	//echo "hex: " .$hex. "<br />";
-	//echo "bin: ".$I->HashAsString($hash, false). "<br />";
-	//echo "visual: ".$I->HashAsTable($hash). "<br />";
 
-	//This is to store facebook id and hashed values for the images in hexa values.
+	//Exclude deleted profiles.
+	if($hex == '0000000000000000'){
+		$initial++;
+	} else{
+		//This is to store facebook id and hashed values for the images in hexa values.
 	mysqli_query($con, "INSERT INTO images(fid, hash) VALUES ('$fid', '$hex')");
 
 	$initial++;
+	}
 }
-
-//Test
-// for($fid=1; $fid < 100; $fid++){
-// $img = @file_get_contents('https://graph.facebook.com/'.$fid.'/picture?type=large');
-// $file = dirname(__file__).'/avatar/'.$fid.'.jpg';
-// file_put_contents($file, $img);
-// }
-
-//This line to print the number of images that has been fetched.
-// $result = $initial-1;
-// echo '<br /><br />Done of fetching ' .$result. ' Pictures';
 
 ?>
